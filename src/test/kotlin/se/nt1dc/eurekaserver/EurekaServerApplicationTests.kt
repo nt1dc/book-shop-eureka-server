fe7f29579b883b9db0a780b4ea32dbbca836c81a
nt1dc <nt1dcare@gmail.com>
2023-03-17 13:45:48 +0300

    init modules

---
+++ src/test/kotlin/se/nt1dc/eurekaserver/EurekaServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package se.nt1dc.eurekaserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
